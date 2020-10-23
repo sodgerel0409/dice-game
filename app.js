17a7730559b4fcf6db0731ec2d7338af7e18f46e
Sodgerel <sodoo2414@gmail.com>
2020-10-23 11:18:10 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -11,14 +11,22 @@ var roundScore = 0;
 
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч 1-6  гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
-var dice = Math.floor(Math.random() * 6) + 1;
+
 // window.document.querySelector('#score-0').textContent = dice;
 
 // window.document.querySelector('#score-1').innerHTML = "<em>YES</em>" 
 
 //Програм эхлэхэд бэлтгэх 
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-window.document.querySelector(".dice").style.display = "none";
+document.getElementById('score-0').textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function()
+{
+    var diceNumber = Math.floor(Math.random() * 6) + 1; 
+    diceDom.style.display = "block";
+    diceDom.src="dice-" + diceNumber + ".png";
+});
